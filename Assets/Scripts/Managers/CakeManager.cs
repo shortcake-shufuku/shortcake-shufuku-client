dc78e2c62313d5b138012affea45af4a77d7a211
Tsubasa Kondo <tsubasa.k111@gmail.com>
2020-02-01 17:05:16 +0900

    feat: add functionality to gameManager, cakeManager, toolmanager, cameraRigControl

---
+++ Assets/Scripts/Managers/CakeManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CakeManager : MonoBehaviour
+{
+    public Transform SpawnPoint;
+
+    [HideInInspector] public GameObject Instance;
+
+    public void Setup()
+    {
+        // TODO
+    }
+
+    public void Reset()
+    {
+        Instance.transform.position = SpawnPoint.position;
+        Instance.transform.rotation = SpawnPoint.rotation;
+
+        Instance.SetActive(false);
+        Instance.SetActive(true);
+    }}
---
+++ Assets/Scripts/Managers/ToolManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolManager
+{
+    public Transform SpawnPoint;
+    public GameObject Prefab;
+
+    [HideInInspector] public GameObject Instance;
+
+    public void Setup()
+    {
+        // TODO
+    }
+
+    public void Reset()
+    {
+        Instance.transform.position = SpawnPoint.position;
+        Instance.transform.rotation = SpawnPoint.rotation;
+
+        Instance.SetActive(false);
+        Instance.SetActive(true);
+    }
+}
